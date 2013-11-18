57fe73d02c268b92fc8aa82b5b276c045aaf7c74
doome1337 <dmitry.paramonov@gmail.com>
2013-11-17 20:44:40 -0500

    Launcher class made. We should begin to remove all other instances of main methods throughout the programs.

---
+++ mgci/jhdap/shogi/Shogi.java
@@ -0,0 +1,14 @@
+package mgci.jhdap.shogi;
+
+/** This class is the launcher for the entire program. Hooray.
+ * @author              Dmitry Andreevich Paramonov
+ * @author              Jiayin Huang
+ */
+public class Shogi {
+    /** This launches the program.
+     * @param   args    We don't use these.
+     */
+    public static void main(String[] args) {
+        new GraphicUI (800, 535);        
+    }
+}
